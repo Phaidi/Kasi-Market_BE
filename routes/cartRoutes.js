2054b32acff4bad2bf66d924c734fb8cb2893d3e
Katleho R Makhoba <katlehormakhoba@gmail.com>
2022-04-21 12:16:28 +0200

    back

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,6 +6,6 @@ const cartController = require('../controllers/cartController')
 router.use(authController.checkUser)
 router.use(cartController.setUserId)
 router.route('/').get(cartController.getAllCarts).post(cartController.createCart)
-
+router.route('/:id').patch(cartController.updateCart).delete(cartController.deleteCart)
 
 module.exports = router;
